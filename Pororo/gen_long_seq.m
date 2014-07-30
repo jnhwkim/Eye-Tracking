1f8b1f2556ad6386d7241aa8e8e76e368524b0c2
Jin Hwa Kim <jnhwkim@gmail.com>
2014-07-30 11:50:47 +0900

    Bug fixed for choosing the correct colormap.

--- Pororo/gen_long_seq.m
+++ Pororo/gen_long_seq.m
@@ -69,6 +69,7 @@ function gen_long_seq
             
         order = 1;
         for j = randsample(1:count, NUM_FIX_SEQ)
+           %% Using parameters
             start_ts = period_table(j,1); end_ts = period_table(j,2);
             if SPANING
                 start_ts = start_ts + (end_ts - start_ts) / 2 - SPAN_LENGTH / 2;
@@ -81,15 +82,17 @@ function gen_long_seq
             out = strcat('img/animated_gif/', participant_id, '/L_', int2str(order), '.gif');
             order = order + 1;
             imout = uint8(zeros(272, 360, 1, size(frames, 4)));
+           %% Colormap for animated-gif
             for k = 1 : size(frames, 4)
                 im = imresize(squeeze(frames(:,:,:,k)), 0.5);
                 if ~exist('imcat', 'var')
-                    imcat = zeros(size(im, 1) * size(frames, 4), size(im, 2), 3);
+                    imcat = uint8(zeros(size(im, 1) * size(frames, 4), size(im, 2), 3));
                 end
                 imcat((size(im, 1) * (k - 1) + 1):size(im, 1) * k, ...
-                    size(im, 2), 3) = im;
+                    :, :) = im;
             end
             [imind, cm] = rgb2ind(imcat, 256,'nodither');
+           %% Making the frames for animation 
             for k = 1 : size(frames, 4)
                 im = imresize(squeeze(frames(:,:,:,k)), 0.5);
                 imout(:,:,1,k) = rgb2ind(im, cm,'nodither');
