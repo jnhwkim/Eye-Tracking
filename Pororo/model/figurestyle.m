51e8d24e6dfd23e9eda627d0d71d0aaa60eeddbf
Jin Hwa Kim <jnhwkim@gmail.com>
2014-10-12 10:34:55 +0900

    Updated the simulation result.

---
+++ Pororo/model/figurestyle.m
@@ -0,0 +1,9 @@
+% Change default axes fonts.
+set(0,'DefaultAxesFontName', 'Palatino')
+set(0,'DefaultAxesFontSize', 13)
+
+% Change default text fonts.
+set(0,'DefaultTextFontname', 'Palatino')
+set(0,'DefaultTextFontSize', 13)
+
+box off;
